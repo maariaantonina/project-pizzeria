1e6112d9507a7bee0f70d995838a2415a31d7b74
Maria Kasińska <mariakasinska17@gmail.com>
2019-10-12 20:30:58 +0200

    debug add to cart and add pages

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,23 @@ import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function () {
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    thisApp.activatePage(thisApp.pages[0].id);
+  },
+  activatePage: function (pageId) {
+    const thisApp = this;
+    //add class active to matching pages, remove from non-matching
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    //add class active to matching links, remove from non-matching
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') == '#' + pageId);
+    }
+  },
   initMenu: function () {
     const thisApp = this;
     for (let productData in thisApp.data.products) {
@@ -36,7 +53,6 @@ const app = {
     thisApp.productList = document.querySelector(select.containerOf.menu);
     thisApp.productList.addEventListener('add-to-cart', function (event) {
       app.cart.add(event.detail.product);
-
     });
   },
   init: function () {
@@ -48,6 +64,7 @@ const app = {
     console.log('templates:', templates);
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initPages();
   },
 };
 
--- src/js/components/Product.js
+++ src/js/components/Product.js
@@ -119,7 +119,7 @@ class Product {
 
     const event = new CustomEvent('add-to-cart', {
       bubbles: true,
-      details: {
+      detail: {
         product: thisProduct,
       }
     });
