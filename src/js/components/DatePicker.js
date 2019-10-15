6ccf8a52168c281a885ad391391a637536e19660
Maria Kasińska <mariakasinska17@gmail.com>
2019-10-15 12:49:14 +0200

    add date picker - does not work

---
+++ src/js/components/DatePicker.js
@@ -0,0 +1,38 @@
+/*global flatpickr*/
+
+
+import BaseWidget from './BaseWidget.js';
+import { select, settings } from '../settings.js';
+import utils from '../utils.js';
+
+
+class DatePicker extends BaseWidget {
+  constructor(wrapper) {
+    super(wrapper, utils.dateToStr(new Date()));
+    const thisWidget = this;
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.input);
+    thisWidget.initPlugin();
+  }
+  initPlugin() {
+    const thisWidget = this;
+    thisWidget.minDate = new Date(thisWidget.value);
+    thisWidget.maxDate = utils.addDays(thisWidget.minDate, settings.datePicker.maxDaysInFuture);
+    flatpickr(thisWidget.dom.input, {
+      defaultDate: thisWidget.minDate,
+      minDate: thisWidget.minDate,
+      maxDate: thisWidget.maxDate,
+      'locale': {
+        'firstDayOfWeek': 1
+      },
+      disable: [function (date) {
+        return (date.getDay() === 1);
+      }],
+      onChange: function (selectedDates, dateStr, instance) {
+        thisWidget.value = dateStr;
+        console.log(thisWidget.value);
+      }
+    });
+  }
+}
+
+export default DatePicker;
